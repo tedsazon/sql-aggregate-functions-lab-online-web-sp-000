adccdccb83ee4a2f13f8c8443fe8fb7bf81042c5
Ted Sazon <tedsazon@gmail.com>
2019-07-16 03:59:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) as average_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) as total_tardies FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
